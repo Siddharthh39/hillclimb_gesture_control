99c0f65817d6a140fb1396124bc327b7cd22ed22
Siddharthh39 <megaberu81@gmail.com>
2025-04-04 13:38:09 +0530

    minor bug fix

--- hillclimb_mode.py
+++ hillclimb_mode.py
@@ -1,10 +1,9 @@
 import cv2
 import mediapipe as mp
-from pynput.keyboard import Controller, Key
+import pyautogui
 import time
 
 def run():
-    keyboard = Controller()
     cap = cv2.VideoCapture(0)
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(max_num_hands=1)
@@ -17,27 +16,18 @@ def run():
         fingers = []
         tips_ids = [8, 12, 16, 20]
         for tip in tips_ids:
-            if landmarks[tip].y > landmarks[tip - 2].y:
-                fingers.append(0)
-            else:
-                fingers.append(1)
+            fingers.append(0 if landmarks[tip].y > landmarks[tip - 2].y else 1)
         return sum(fingers) == 0
 
     def is_open_hand(landmarks):
         fingers = []
         tips_ids = [8, 12, 16, 20]
         for tip in tips_ids:
-            if landmarks[tip].y < landmarks[tip - 2].y:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+            fingers.append(1 if landmarks[tip].y < landmarks[tip - 2].y else 0)
         return sum(fingers) == 4
 
     while True:
         success, frame = cap.read()
-        if not success:
-            break
-
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         result = hands.process(frame_rgb)
 
@@ -48,33 +38,42 @@ def run():
 
                 if is_open_hand(landmark_list):
                     if not pressing_gas:
-                        keyboard.press(Key.right)
+                        pyautogui.keyDown("right")
                         pressing_gas = True
                         pressing_brake = False
-                        keyboard.release(Key.left)
+                        pyautogui.keyUp("left")
                         print("Gas")
+                    time.sleep(0.1)
+
                 elif is_fist(landmark_list):
                     if not pressing_brake:
-                        keyboard.press(Key.left)
+                        pyautogui.keyDown("left")
                         pressing_brake = True
                         pressing_gas = False
-                        keyboard.release(Key.right)
+                        pyautogui.keyUp("right")
                         print("Brake")
+                    time.sleep(0.1)
+
                 else:
-                    keyboard.release(Key.left)
-                    keyboard.release(Key.right)
+                    pyautogui.keyUp("left")
+                    pyautogui.keyUp("right")
                     pressing_brake = False
                     pressing_gas = False
 
         else:
-            keyboard.release(Key.left)
-            keyboard.release(Key.right)
+            # Debug: When no hand landmarks are detected
+            pyautogui.keyUp("left")
+            pyautogui.keyUp("right")
             pressing_brake = False
             pressing_gas = False
 
         cv2.imshow("Gesture Controller", frame)
         if cv2.waitKey(1) & 0xFF == 27:
+            print("Exiting...")
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    run()
